7d391b3133e667fb793dbeab910a293614b13c80
kylekrieger <kylekrieger3404@gmail.com>
2018-10-24 23:06:05 -0400

    Hi

--- python/sandbox/basic.py
+++ python/sandbox/basic.py
@@ -1,6 +1,8 @@
 import sys
 
 def average(scores):
+    if len(scores) == 0 :
+        return 0.0
     sum = 0.0
     for score in scores:
         sum += score
@@ -20,4 +22,9 @@ print("AVERAGE OF " + str(len(scores)) + " : " + str(average(scores)))
 print("AVERAGE OF " + str(len(evan)) + " : " + str(average(evan)))
 
 
+alex = [91.0, 89.0, 77.0, 96.0,94.0]
 
+print("AVERAGE OF " +  str(len(alex)) + " : " + str(average(alex)))
+
+david = []
+print("AVERAGE OF " +  str(len(david)) + " : " + str(average(david)))
