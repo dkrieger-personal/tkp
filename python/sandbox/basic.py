e7d69ec0f502877db116c2720cccc83b8bc5bf57
David <davidkrieger.chocolate@gmail.com>
2018-10-24 19:49:08 -0700

    test commit

--- python/sandbox/basic.py
+++ python/sandbox/basic.py
@@ -1,4 +1,23 @@
 import sys
 
+def average(scores):
+    sum = 0.0
+    for score in scores:
+        sum += score
+    average = sum / len(scores)
+    return average
+
+
 print(sys.version)
 print("HI MOM!")
+
+# grade average
+scores = [98.0, 100.0, 100.0, 97.0, 99.0, 95.0,99.0, 100.0]
+evan = [78.0, 90.0, 85.0, 88.9]
+
+
+print("AVERAGE OF " + str(len(scores)) + " : " + str(average(scores)))
+print("AVERAGE OF " + str(len(evan)) + " : " + str(average(evan)))
+
+
+
