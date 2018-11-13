db72a2dd73faa5934d931d580cfe964464da9e40
kylekrieger <kylekrieger3404@gmail.com>
2018-11-12 21:52:43 -0500

    hi

--- python/sandbox/hangman.py
+++ python/sandbox/hangman.py
@@ -1,1 +1,16 @@
+import random
 print("hangman!")
+#choose a word (display length)
+#guess a letter
+#determine whether it's right or wrong
+#insert the correctly guessed letters
+#count tbe incorrect guesses until it has exceeded its maximum
+#continue guessing and repeat
+def getword ():
+    words= ["green", "yellow", "chicken","gentlemen"]
+    i=random.randint (0,len(words)-1)
+    return words[i]
+
+for i in range(0,10):
+    print (getword())
+    
