01fdc7de4e56aba09aef9f1da9a99b3d4e8b07c7
kylekrieger <kylekrieger3404@gmail.com>
2018-12-02 10:17:40 -0500

    hi

--- python/sandbox/hangman.py
+++ python/sandbox/hangman.py
@@ -11,6 +11,68 @@ def getword ():
     i=random.randint (0,len(words)-1)
     return words[i]
 
-for i in range(0,10):
-    print (getword())
-    
+def getguess ():
+    guess= input('guess-->')
+    return guess
+
+def newindex (character):
+    return ord(character)-97
+
+#choose a word (display length, initialize game)
+ThisGameWord = getword()
+#print(ThisGameWord, len(ThisGameWord))
+print('Your word has this many letters: ',len(ThisGameWord))
+
+#guess a letter and put it in a variable
+MyGuess = [] #array of guesses
+youwin = 0 #indicates if you have won
+g=0 #counter of guesses
+
+a_val = ord('a')
+for k in range (a_val,a_val+26):
+    MyGuess.append(0)
+    # print (k,a_val,MyGuess[k-a_val],chr(k))
+
+
+while youwin == 0:
+    currentguess=getguess()
+    gotit = 0
+    if (MyGuess[newindex(currentguess)]>0):
+        print ("You already guessed that")
+    else:
+        for i in range (0,(len(ThisGameWord))):
+            if (currentguess==ThisGameWord[i]):
+                if gotit == 0:
+                    print ("Yes, new correct letter!")
+                    gotit=1
+                MyGuess [newindex(currentguess)]=1
+        if gotit == 0:
+            print ("Sorry, that letter is incorrect")
+
+    correctcount = 0
+    for i in range (0,(len(ThisGameWord))):
+        if (MyGuess[newindex(ThisGameWord[i])])>0:
+            print(ThisGameWord[i],end='')
+            correctcount+=1
+        else:
+            print('-',end='')
+
+    print('')
+    if correctcount == len(ThisGameWord):
+        print('You win!')
+        youwin=1
+    else:
+        g+=1
+
+
+
+#insert the correctly guessed letters
+#count the incorrect guesses until it has exceeded its maximum
+#continue guessing and repeat
+
+
+
+#for i in range(1,5):
+#    print ('Guess #',i,':',getguess())
+
+
