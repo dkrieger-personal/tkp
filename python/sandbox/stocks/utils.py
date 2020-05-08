4d23b41a8e9ee4f870292f359c8069c9f3848283
David <davidkrieger.chocolate@gmail.com>
2020-05-08 11:47:48 -0700

    added risk measures

--- python/sandbox/stocks/utils.py
+++ python/sandbox/stocks/utils.py
@@ -1,9 +1,10 @@
+import pprint
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as wb
 import matplotlib.pyplot as plt
 import math
-from typing import List
+from typing import List, Dict
 
 
 def daily(tickers: List[str], start: str, end: str) -> pd.DataFrame:
@@ -23,6 +24,25 @@ def plot_daily(daily: pd.DataFrame) -> None:
     plt.show()
 
 
+def daily_log(daily: pd.DataFrame) -> pd.DataFrame:
+    return np.log(daily/daily.shift(1))
+
+
+def risk(daily_log: pd.DataFrame) -> Dict[str, Dict[str, float]]:
+    risk = {}
+    for ticker,value in daily_log.to_dict().items():
+        if ticker not in risk:
+            risk[ticker] = {}
+        risk[ticker]['daily_mean'] = daily_log[ticker].mean()
+        risk[ticker]['annual_mean'] = risk[ticker]['daily_mean'] * 250
+        risk[ticker]['daily_std'] = daily_log[ticker].std()
+        risk[ticker]['annual_std'] = risk[ticker]['daily_std'] * (250 ** 0.5)
+    return risk
+
+
 if __name__ == "__main__":
-    d = daily(['^GSPC', '^IXIC', '^GDAXI'], '2000-01-01', '2020-05-06')
-    print(d.tail())
+    d = daily(['PG', 'BEI.DE'], '2007-01-01', '2020-05-03')
+    plot_daily(d)
+    ld = daily_log(d)
+    print(ld)
+    pprint.pprint(risk(ld))
--- python/sandbox/stocks/utils_test.py
+++ python/sandbox/stocks/utils_test.py
@@ -23,6 +23,31 @@ class TestDaily(unittest.TestCase):
         self.assertAlmostEqual(annual['F'], 0.0335, places=4)
         self.assertAlmostEqual(annual['MSFT'], 0.1229, places=4)
 
+    def test_risk(self):
+        expected = {
+            'BEI.DE': {
+                'annual_mean': 0.05978,
+                'annual_std': 0.21762,
+                'daily_mean': 0.00024,
+                'daily_std': 0.01376
+            },
+            'PG': {
+                'annual_mean': 0.07423,
+                'annual_std': 0.18887,
+                'daily_mean': 0.00030,
+                'daily_std': 0.01194
+            }
+        }
+
+        daily = utils.daily(['PG', 'BEI.DE'], '2007-01-01', '2020-05-03')
+        actual = utils.risk(utils.daily_log(daily))
+        for ticker, stats in expected.items():
+            self.assertTrue(ticker in actual)
+            for stat, value in stats.items():
+                self.assertTrue(stat in actual[ticker])
+                self.assertAlmostEqual(actual[ticker][stat], value, places=5)
+
+
 
 if __name__ == '__main__':
     unittest.main()
