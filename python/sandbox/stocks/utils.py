70b2f188fcf64780d07b38f5ec4917a702a4b42d
David <davidkrieger.chocolate@gmail.com>
2020-05-12 16:46:48 -0700

    add linear regression

--- python/sandbox/stocks/utils.py
+++ python/sandbox/stocks/utils.py
@@ -5,6 +5,8 @@ from pandas_datareader import data as wb
 import matplotlib.pyplot as plt
 import math
 from typing import List, Dict
+from scipy import stats
+import statsmodels.api as sm
 
 
 def daily(tickers: List[str], start: str, end: str) -> pd.DataFrame:
@@ -60,9 +62,38 @@ def portfolio(daily_log: pd.DataFrame, weights: np.array) -> (float, float, floa
     return mean, vol, dr, var - dr
 
 
+def linear_regression(X: pd.Series, Y: pd.Series) -> (float, float, float):
+    slope, intercept, r_value, p_value, std_err = stats.linregress(X, Y)
+    return [intercept, slope, r_value ** 2]
+
+def plot_lr(X: pd.Series, Y: pd.Series, xlabel, ylabel) -> None:
+    # scale plot to 110% of max values
+    maxX = np.max(X) * 1.1
+    maxY = np.max(Y) * 1.1
+
+    intercept, slope, rvalue = linear_regression(X, Y)
+
+    # show raw data
+    plt.scatter(X, Y)
+    plt.axis([0, maxX, 0, maxY])
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    # add in regression line
+    x_points = [0, maxX]
+    y_points = [intercept, intercept + slope * maxX]
+    plt.plot(x_points, y_points, marker='x')
+    plt.annotate("R2="+str(rvalue), (maxX * 0.1, maxY * 0.9))
+
+    plt.show()
+
+
 if __name__ == "__main__":
-    d = daily(['PG', 'BEI.DE'], '2010-01-01', '2020-05-07')
-    plot_daily(d)
+    housing = pd.read_excel(r'C:\Users\David\code\udemy\pyfi\stock\81 Running a Regression in Python\Python 3\Housing.xlsx')
 
+    xlabel = 'House Size (sq.ft.)'
+    ylabel = 'House Price'
+    X = housing[xlabel]
+    Y = housing[ylabel]
 
-
+    plot_lr(X, Y, xlabel, ylabel)
